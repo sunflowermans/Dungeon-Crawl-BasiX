1062467b36ca1a84c2bc144195e1c1f9c49a50e5
sunflowermans <directsun@gmail.com>
2021-07-25 13:31:24 -0700

    legacy torch fix dependancy set up

--- scripts/prototype/lighttorch.js
+++ scripts/prototype/lighttorch.js
@@ -7,23 +7,25 @@ resources.forEach((r)=> {
    }
 });
 
-let name = "torch_" + torchNumber;
+let torchName = "torch_" + torchNumber;
 
-console.log(name);
+console.log(torchName);
 
-resources.push(name)
+resources.push(torchName)
 window.pr.api.set('resource_list', resources)
 
-window.pr.api.register(name);
-window.pr.api.register(name.concat('_name'));
-window.pr.api.register(name.concat('_visible'), { default: true });
-window.pr.api.register(name.concat('_max'));
-window.pr.api.register(name.concat('_min'));
-window.pr.api.register(name.concat('_player_managed'), { default: false });
+window.pr.api.register_setting(torchName);
+window.pr.api.register_setting(torchName.concat('_name'));
+window.pr.api.register_setting(torchName.concat('_visible'), { default: true });
+window.pr.api.register_setting(torchName.concat('_max'));
+window.pr.api.register_setting(torchName.concat('_min'));
+window.pr.api.register_setting(torchName.concat('_player_managed'), { default: false });
+window.pr.api.register_setting(torchName.concat('_notify_chat'), { default: false })
 
-window.pr.api.set(name, 60);
-window.pr.api.set(name.concat('_name'), name);
-window.pr.api.set(name.concat('_visible'), true);
-window.pr.api.set(name.concat('_max'), 60);
-window.pr.api.set(name.concat('_min'), 0);
-window.pr.api.set(name.concat('_player_managed'), false);
+window.pr.api.set(torchName, 60);
+window.pr.api.set(torchName.concat('_name'), torchName);
+window.pr.api.set(torchName.concat('_visible'), true);
+window.pr.api.set(torchName.concat('_max'), 60);
+window.pr.api.set(torchName.concat('_min'), 0);
+window.pr.api.set(torchName.concat('_player_managed'), false);
+window.pr.api.set(torchName.concat('_notify_chat'), false);
